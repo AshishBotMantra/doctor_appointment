6563bc9bbf11d0c35a7339dd850317239a22b8df
AshishBotMantra <89506034+AshishBotMantra@users.noreply.github.com>
2021-08-31 14:43:17 +0530

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-from .config import *
-from .users import user
-from .patient import patient
-
-app.register_blueprint(user,url_prefix="")
-app.register_blueprint(patient,url_prefix="")
